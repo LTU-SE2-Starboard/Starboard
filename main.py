ff2f4b5d736c66eb339ada6759dc66dd93e4c134
Sasha Koshka <supergriffin64@gmail.com>
2022-04-18 14:08:54 -0400

    UI test

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import kivy
 kivy.require('2.1.0') # replace with your current kivy version !
 
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.widget import Widget
 
+class StarboardInterface (Widget):
+        pass
 
 class Starboard (App):
-
-    def build (self):
-        return Label(text='Hello world')
+        def build (self):
+                return StarboardInterface()
 
 
 if __name__ == '__main__':
-    Starboard().run()
+        Starboard().run()
