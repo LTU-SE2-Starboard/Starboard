cfcd7235f1fadb78fbbaac8f154f368f0eb34590
cmulholland <99295376+cmulholla@users.noreply.github.com>
2022-04-20 14:36:24 -0400

    Update main.py
    got rid of the red dot of doom

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import kivy
 kivy.require('2.1.0') # replace with your current kivy version !
 
+#this gets rid of the red dot of doom
+from kivy.config import Config
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 
