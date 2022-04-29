442f202c38d87994456df2d741eea182e0acb4ad
Sasha Koshka <supergriffin64@gmail.com>
2022-04-29 02:11:24 -0400

    Polished page layout a bit

--- main.py
+++ main.py
@@ -9,21 +9,33 @@ Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.textinput import TextInput
+from kivy.uix.pagelayout import PageLayout
 from kivy.properties import StringProperty
 from kivy.properties import ObjectProperty
 
-class Starboard (App):
-        pages = ObjectProperty(None)
+class StarboardInterface (Widget):
+        pass
 
+class NotePagesWidget (PageLayout):
+        pass
+
+class Starboard (App):
         def build (self):
+                interface = StarboardInterface()
+                
                 os.makedirs(os.path.expanduser("~/.starboard/notes"), exist_ok = True)
                 os.makedirs(os.path.expanduser("~/.starboard/templates"), exist_ok = True)
 
                 notes = {}
         
                 root = self.root
-                # self.pages.add_widget(NoteWidget(text = "Sample text"))
-                pass
+                pages = NotePagesWidget()
+                interface.add_widget(pages)
+                pages.add_widget(NoteWidget(text = "Note1"))
+                pages.add_widget(NoteWidget(text = "Note2"))
+                pages.add_widget(NoteWidget(text = "Note324234"))
+                pages.add_widget(NoteWidget(text = "Not3243244334234"))
+                return interface
 
         def createNote (template = None):
                 pass
